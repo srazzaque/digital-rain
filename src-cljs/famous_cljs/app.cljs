cf16edff7091671dfeacfe6b571369946a816910
Sandipan Razzaque <me@sandipan.net>
2015-03-09 21:47:54 +1100

    Remove unneeded code and assets

--- src-cljs/famous_cljs/app.cljs
+++ src-cljs/famous_cljs/app.cljs
@@ -1,21 +1,7 @@
 (ns famous-cljs.app
-  (:require [famous-cljs.navigation :as nav]
-            [famous-cljs.pages :as pages]
-            [famous-cljs.matrix :as matrix]
-            [cljs.core.async :refer [<!]])
-  (:require-macros [cljs.core.async.macros :refer [go-loop]]))
+  (:require [famous-cljs.matrix :as matrix]))
 
 (enable-console-print!)
-
-(defn ^:export start-app
-  []
-  (println "Starting up.")
-  (let [context (.createContext js/famous.core.Engine)
-        layout (js/famous.views.HeaderFooterLayout. #js{:headerSize 100
-                                                        :footerSize 50})
-        nav-chan (nav/add-navigation (.-header layout))]
-    (pages/add-pages (.-content layout) nav-chan)
-    (.add context layout)))
 
 (defn ^:export start
   []
